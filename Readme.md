1142c2ae60d11a56d27fab72e1271f42f5bfd953
hitesh-s0lanki <hiteshsolanki4623@gmail.com>
2025-06-03 18:27:32 +0530

    - kafka broker - spring stream - spring cloud - spring cloud function

---
+++ Readme.md
@@ -0,0 +1,6 @@
+1. Kafka Basic -> Topic, Partition, Broker, Zookeeper
+2. Kafka Docker command
+3. Serializer & Deserializer
+4. Spring Cloud Function
+5. Spring Cloud Stream
+6.
--- consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.kafka.consumer;
 
+import com.kafka.consumer.model.RiderLocation;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,11 @@ public class KafkaConsumer {
     @KafkaListener(topics = "my-topic", groupId = "my-new-group2")
     public void listen2(String message){
         System.out.println("Received Message 2 : "+ message);
+    }
+
+    @KafkaListener(topics = "my-topic-new", groupId = "my-group")
+    public void listen3(RiderLocation location){
+        System.out.println("Received Message 2 : "+ location.getRiderId());
     }
 
 }
